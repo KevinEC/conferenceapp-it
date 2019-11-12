4ac0592ee58a43318750ad4651e1e7ecf7aa7788
kevinec <k.evans.c@gmail.com>
2019-11-12 18:21:01 +0000

    Firebase setup. Configuring Reactcontext/provider structure for easy Firebase imports

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 
-export default class Home extends React.Component {
+import { withFirebase } from "../Firebase";
+
+class Home extends React.Component {
 
 	constructor(props) {
 		super(props);
+
+		this.log = this.log.bind(this);
+	}
+
+	componentDidMount(){
+		this.props.firebase.addDummyData();
+	}
+
+	log() {
+		const events = this.props.firebase.getAll('users');
+		console.log("Events: ", events);
 	}
 
 	render() {
 		return (
-			<h1>home component</h1>
+			<div>
+				<h1>home component</h1>
+				<button onClick={this.log}>{"console.log"}</button>
+			</div>
 		);
 	}
 }
+export default withFirebase(Home);
--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,21 +1,55 @@
-import app from 'firebase/app';
+import firebase from 'firebase';
 
 const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-  	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  	databaseURL: process.env.REACT_APP_DATABASE_URL,
-  	projectId: process.env.REACT_APP_PROJECT_ID,
-  	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    // more than what the tutorial has
-    appId: process.env.REACT_APP_ID,
-    measurementId: process.env.REACT_MEASUREMNT_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  // more than what the tutorial has
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_MEASUREMNT_ID
 };
 
 class Firebase {
 	constructor() {
-	  app.initializeApp(config);
-	}
+   firebase.initializeApp(config);
+
+   this.db = firebase.firestore();
+ }
+
+ getAll = (document) => {
+  let result;
+
+    /* this.db.collection("events")
+    .get()
+    .then(querySnapshot => {
+      result = querySnapshot.docs.map(doc => doc.data());
+    });*/
+
+    this.db.collection(document).get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+      });
+    });
+
+    return result;
+  }
+
+  addDummyData = () => {
+    this.db.collection("users").add({
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815
+    })
+    .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+      console.error("Error adding document: ", error);
+    });
+  }
 }
 
 export default Firebase;
