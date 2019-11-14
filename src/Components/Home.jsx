d4159f1e604a4519e300b1ff01e353289e3d1a8f
propeus00 <57322492+propeus00@users.noreply.github.com>
2019-11-14 13:51:39 +0000

    Chnage header home message

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,27 +1,26 @@
-import React from 'react';
+import React from "react";
 
 import { withFirebase } from "../Firebase";
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
 
-	constructor(props) {
-		super(props);
+    this.log = this.log.bind(this);
+  }
 
-		this.log = this.log.bind(this);
-	}
+  async log() {
+    const events = await this.props.firebase.db.getAll("events");
+    console.log("Events: ", events);
+  }
 
-	async log() {
-		const events = await this.props.firebase.db.getAll('events');
-		console.log("Events: ", events);
-	}
-
-	render() {
-		return (
-			<div>
-				<h1>home component</h1>
-				<button onClick={this.log}>{"console.log"}</button>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <h1>home Change happen here</h1>
+        <button onClick={this.log}>{"console.log"}</button>
+      </div>
+    );
+  }
 }
 export default withFirebase(Home);
