4652d7bd68307d23a7a662c7e816bb28dc150e84
propeus00 <57322492+propeus00@users.noreply.github.com>
2019-11-15 19:08:44 +0000

    Reintroduced app.jsx header AuthUserContext

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ import './App.css';
 
 import Home from "./Components/Home.jsx";
 import SignInPage from "./Components/SignIn";
-import Tickets from "./Components/Tickets/Tickets"
+import Tickets from "./Components/Tickets/Tickets";
+import { AuthUserContext, withAuthentication } from "./Middleware/Session";
 import 'semantic-ui-css/semantic.min.css';
 import * as ROUTES from "./Routes/routes";
 
