7a4d21ce6f46ed6b473b03ab40f8be19fe6c3723
kevinec <k.evans.c@gmail.com>
2019-11-21 18:21:28 +0000

    merged

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Route,
 } from 'react-router-dom';
+//import 'semantic-ui-css/semantic.min.css';
 
 import './App.less';
 import { Container } from 'semantic-ui-react';
@@ -15,7 +16,6 @@ import Home from "./Components/Home";
 import Tickets from "./Components/Tickets/Tickets";
 import {RoomsEvent} from "./Components/RoomsEvent/RoomsEvent"
 import { AuthUserContext, withAuthentication } from "./Middleware/Session";
-import 'semantic-ui-css/semantic.min.css';
 
 import Events from "./Components/Events";
 import Keynote from "./Components/Events/Keynote";
@@ -37,11 +37,7 @@ const App = () => {
           <Route path={ROUTES.TICKETS} component={Tickets} />
           <Route exact path={ROUTES.EVENTS} component={Events} />
           <Route path={ROUTES.KEYNOTE} component={Keynote} />
-
-            <Route path={ROUTES.TICKETS} >
-              <Tickets authenticated={authUser}/>
-            </Route>
-            <Route path={ROUTES.ROOMS} component={RoomsEvent} />
+          <Route path={ROUTES.ROOMS} component={RoomsEvent} />
           
           <Route path={ROUTES.SIGN_IN} component={SignInPage} />
         </Router>
